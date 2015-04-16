cffd37f4706c05162e2495c9b7ac3349bcf1f8b1
garyelephant <garygaowork@gmail.com>
2015-04-16 15:24:26 +0800

    replace bcolors class with colors class

--- highlight.py
+++ highlight.py
@@ -1,12 +1,45 @@
-class bcolors:
-    HEADER = '\033[95m'
-    OKBLUE = '\033[94m'
-    OKGREEN = '\033[92m'
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
-    ENDC = '\033[0m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
+class colors:
+    '''Colors class:
+    reset all colors with colors.reset
+    two subclasses fg for foreground and bg for background.
+    use as colors.subclass.colorname.
+    i.e. colors.fg.red or colors.bg.green
+    also, the generic bold, disable, underline, reverse, strikethrough,
+    and invisible work with the main class
+    i.e. colors.bold
+    '''
+    reset='\033[0m'
+    bold='\033[01m'
+    disable='\033[02m'
+    underline='\033[04m'
+    reverse='\033[07m'
+    strikethrough='\033[09m'
+    invisible='\033[08m'
+    class fg:
+        black='\033[30m'
+        red='\033[31m'
+        green='\033[32m'
+        orange='\033[33m'
+        blue='\033[34m'
+        purple='\033[35m'
+        cyan='\033[36m'
+        lightgrey='\033[37m'
+        darkgrey='\033[90m'
+        lightred='\033[91m'
+        lightgreen='\033[92m'
+        yellow='\033[93m'
+        lightblue='\033[94m'
+        pink='\033[95m'
+        lightcyan='\033[96m'
+    class bg:
+        black='\033[40m'
+        red='\033[41m'
+        green='\033[42m'
+        orange='\033[43m'
+        blue='\033[44m'
+        purple='\033[45m'
+        cyan='\033[46m'
+        lightgrey='\033[47m'
 
 def highlight( s, **term_color ):
     """return ANSI color rendered string
@@ -24,7 +57,7 @@ def highlight( s, **term_color ):
             start = term_start + 1
 
             color_pos[ term_start ] = color
-            color_pos[ term_end ] = bcolors.ENDC
+            color_pos[ term_end ] = colors.reset
 
     if len( color_pos ) == 0:
         return s 
@@ -46,8 +79,8 @@ if __name__ == '__main__':
          "The most common way to do this is by printing ANSI escape sequences."
 
     terms_color = {
-                      'print string with color': bcolors.WARNING,
-                      'platform': bcolors.OKBLUE,
-                      'ANSI escape sequences': bcolors.FAIL
+                      'print string with color': colors.fg.yellow,
+                      'platform': colors.fg.blue,
+                      'ANSI escape sequences': colors.fg.red
                   }
     print highlight( s, **terms_color )
